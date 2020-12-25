c324a665a993df9de740a94e02844b024208f664
peterpeterparker <david.dalbusco@outlook.com>
2020-12-25 10:22:11 +0100

    feat: video

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import {useEffect, useRef} from 'react';
+
 import Head from 'next/head';
+
 import styles from '../styles/Home.module.scss';
 
 const Home = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    init();
+  }, [videoRef]);
+
+  const init = async () => {
+    if (!videoRef?.current) {
+      return;
+    }
+
+    if (!navigator.mediaDevices?.getUserMedia) {
+      return;
+    }
+
+    const stream: MediaStream = await navigator.mediaDevices.getUserMedia({audio: false, video: {width: 1280, height: 720}});
+
+    const video: HTMLVideoElement = (videoRef.current as unknown) as HTMLVideoElement;
+
+    video.srcObject = stream;
+    await video.play();
+  };
+
   return (
     <>
       <Head>
@@ -9,7 +35,10 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Hello World</main>
+      <main className={styles.main}>
+        Hello World
+        <video ref={videoRef}></video>
+      </main>
     </>
   );
 };
