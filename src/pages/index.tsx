f4beeba9d814685758dc5ac207881ec72e5ac66b
peterpeterparker <david.dalbusco@outlook.com>
2020-12-31 17:41:29 +0100

    feat: add icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,17 @@ const Index = () => {
     <>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
 
         <meta name="viewport" content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#004e64" />
+        <meta name="msapplication-TileColor" content="#00a5cf" />
+        <meta name="theme-color" content="#000501" />
+
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <Home></Home>
