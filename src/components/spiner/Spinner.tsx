b7d7fd3f5e8e2ff2e838a35a23b58213916c73b6
peterpeterparker <david.dalbusco@outlook.com>
2021-01-03 08:52:21 +0100

    fix: jsx

--- src/components/spiner/Spinner.tsx
+++ src/components/spiner/Spinner.tsx
@@ -3,7 +3,7 @@ import styles from './Spinner.module.scss';
 const Spinner = () => {
   return (
     <svg viewBox="0 0 36 36" className={`${styles.spinner}`}>
-      <circle cx="18" cy="18" r="15" stroke-width="4"></circle>
+      <circle cx="18" cy="18" r="15" strokeWidth="4"></circle>
     </svg>
   );
 };
